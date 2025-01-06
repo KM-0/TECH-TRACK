e084677f6af5f8abcd734ce70165e532291fe9bc
Keita Maeda <laughlaughlife@outlook.jp>
2025-01-06 23:07:45 +0900

    themeの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+// import { Icon } from "@/components/atoms/Icon";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}>Tech Stack</div>;
+  return (
+    <>
+      <div className={styles.page}>Tech Stack</div>
+      <div style={{ width: "300px" }}>{/* <Icon name="arrowPath" /> */}</div>
+    </>
+  );
 }
