a5169d737a3364afe6416e9ac958ac2821df1460
Keita Maeda <laughlaughlife@outlook.jp>
2025-01-14 23:07:27 +0900

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      <div className={styles.page}>Tech Stack</div>
+      <div className={styles.page}>Tech Stack ADD</div>
       <div style={{ width: "300px" }}>{/* <Icon name="arrowPath" /> */}</div>
     </>
   );
