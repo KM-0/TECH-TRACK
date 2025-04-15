81efdc19008f84cd0de327241d1cbf797243ebd4
Keita Maeda <laughlaughlife@outlook.jp>
2025-04-15 23:05:48 +0900

    chore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+// import AcmeLogo from "../app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
   return (
-    <>
-      <div className={styles.page}>Tech Stack ADD</div>
-      <div style={{ width: "300px" }}>{/* <Icon name="arrowPath" /> */}</div>
-    </>
+    <main className={styles.mainContainer}>
+      <div className={styles.header}>{/* <AcmeLogo /> */}</div>
+      <div className={styles.contentContainer}>
+        <div className={styles.leftPanel}>
+          <p className={styles.welcomeText}>Welcome!!</p>
+          <Link href="/login" className={styles.loginButton}>
+            <span>Log in</span> <ArrowRightIcon className={styles.arrowIcon} />
+          </Link>
+        </div>
+        <div className={styles.rightPanel}>{/* Add Hero Images Here */}</div>
+      </div>
+    </main>
   );
 }
