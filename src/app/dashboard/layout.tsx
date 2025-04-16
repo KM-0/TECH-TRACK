6197af6667305b3c7dc0731644059336ad68b967
Keita Maeda <laughlaughlife@outlook.jp>
2025-04-16 23:41:07 +0900

    LinkButtonの修正

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,18 @@
 // import SideNav from "@/app/ui/dashboard/sidenav";
+import { LinkButton } from "@/components/atoms/LinkButton";
 import styles from "./dashboard-layout.module.css";
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>{/* <SideNav /> */}</div>
+      <div className={styles.sidebar}>
+        <LinkButton linkText="Home" href={"/"} icon={<HomeIcon />}></LinkButton>
+      </div>
       <div className={styles.content}>{children}</div>
     </div>
   );
--- src/components/atoms/LinkButton/index.tsx
+++ src/components/atoms/LinkButton/index.tsx
@@ -1,5 +1,6 @@
 import Link, { LinkProps } from "next/link";
 import { ReactNode } from "react";
+import styles from "./style.module.css";
 
 type Props = {
   linkText: string;
@@ -8,9 +9,9 @@ type Props = {
 
 export const LinkButton: React.FC<Props> = ({ linkText, icon, href }) => {
   return (
-    <Link href={href}>
-      <div>
-        <div>{icon}</div>
+    <Link href={href} className={styles.container}>
+      <div className={styles.wrapper}>
+        <div style={{ width: 20 }}>{icon}</div>
         <div>{linkText}</div>
       </div>
     </Link>
